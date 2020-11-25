a9c4da5b112db00e1124b66f72205f19b6dfc8f7
MaverickBirdman <73669666+MaverickBirdman@users.noreply.github.com>
2020-11-25 23:06:42 +0100

    Update transportujFlota.hpp

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -2,32 +2,26 @@
 
 #include "Stocznia.hpp"
 unsigned int transportujFlota(unsigned int towar)
-{
-	int unsigned liczbaZaglowcow=0;
-	int unsigned licznikPrzetransportowanegoSumarycznieTowaru=0;
-	//#dod
-
+		{
+int unsigned liczbaZaglowcow=0;
+int unsigned licznikPrzetransportowanegoSumarycznieTowaru=0;
 	if (towar == 0)
-		{
-		return 0;
-		}
+{
+	return 0;
+}
 	else
-		{
-		while (licznikPrzetransportowanegoSumarycznieTowaru < towar)
-		{
-	//#1
-			Stocznia stocznia{};
-			Statek* s1 = stocznia();
-	//#2 + #3
-			licznikPrzetransportowanegoSumarycznieTowaru = licznikPrzetransportowanegoSumarycznieTowaru + s1->transportuj();
-	//#dod
+	{
+	while (licznikPrzetransportowanegoSumarycznieTowaru < towar)
+{
+Stocznia stocznia{};
+Statek* s1 = stocznia();
+licznikPrzetransportowanegoSumarycznieTowaru = licznikPrzetransportowanegoSumarycznieTowaru + s1->transportuj();
 	if (s1 == dynamic_cast<Zaglowiec*>(s1))
-		{
-		liczbaZaglowcow = ++liczbaZaglowcow;
-		}
-	//#4
+	{
+	liczbaZaglowcow = ++liczbaZaglowcow;
+	}
 	delete s1;
-		}
+}
 	}
 return liczbaZaglowcow;
-}
+		}
